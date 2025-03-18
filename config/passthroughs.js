d3b00c60b10e461930ebad393b52d24e1da9a91d
DanielSrb <srb.dan@gmail.com>
2025-03-18 19:56:27 +0100

    fix: admin js in production

--- config/passthroughs.js
+++ config/passthroughs.js
@@ -7,4 +7,7 @@ export default (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy('static/admin')
     eleventyConfig.addPassthroughCopy('static/assets')
     eleventyConfig.addPassthroughCopy('src/assets')
+    eleventyConfig.addPassthroughCopy({
+        'node_modules/@sveltia/cms/dist/sveltia-cms.js': 'static/admin/sveltia-cms.js'
+    })
 }
